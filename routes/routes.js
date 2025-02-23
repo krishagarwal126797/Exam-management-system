b250b104067799b2753adf7b6da7e624c7d4dd82
Krish Agarwal <142911439+krishagarwal126797@users.noreply.github.com>
2025-02-23 12:41:50 +0530

    ADD REGISTER

--- routes/routes.js
+++ routes/routes.js
@@ -6,5 +6,15 @@ router.get("/", async (req, res) => {
     res.render('Login', { title: 'Login Page' });
 
   
+}
+
+
+);
+router.get("/Register", async (req, res) => {
+ 
+   
+    res.render('Register', { title: 'Register Page' });
+
+  
 });
 export default router;
